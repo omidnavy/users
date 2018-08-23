2d0bf4ee7ac06619b82c854dece60a18f244762b
Omid Navy <omidnavy@yahoo.com>
2018-08-23 21:11:03 +0430

    database config added

--- app/core/BaseDBModel.js
+++ app/core/BaseDBModel.js
@@ -2,20 +2,21 @@
 Simple MYSQL DB Class using pools and basic query function.
 you can extend this class as you need or change it to another DB
  */
+const config = require('./config/database').home;
+
+
 const bluebird = require('bluebird');
 const redis = require('redis');
 bluebird.promisifyAll(redis);
 
-const client = redis.createClient({host: "192.168.0.7"});
+const client = redis.createClient({host: config.host});
 client.on('connect', () => console.log('redis connected\n'));
 client.on('error', (e) => logger('error', e));
 
 
 const mongojs = require('mongoist');
-const url = "mongodb://192.168.0.7:27017/users";
-const config = require('./config/database').development;
+const url = `mongodb://${config.host}:27017/users`;
 const BaseModel = require('./BaseModel');
-
 const db = mongojs(url, {useNewUrlParser: true});
 const onConnectionError = (e) => {
     logger('error', e);
